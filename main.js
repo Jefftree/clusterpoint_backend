91824ed4d72b337725b9ca8fbab2979241e67f75
Jeffrey Ying <jeffrey.ying86@live.com>
2015-06-13 15:45:35 -0400

    port

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ server.get('/sportslist', function (req, res, next) {
         return next()
     })
 })
-
-server.listen(8080, function () {
+var port = process.env.PORT || 8080;
+server.listen(port, function () {
     console.log('%s listening at %s', server.name, server.url)
 })
